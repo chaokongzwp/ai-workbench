93ea57ea634bea10681e798f460aa2bb45396ed3
zwp <105546397@qq.com>
2026-07-27 10:40:37 +0800

    refactor(ui): unify Mac visual language

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
+import "./design-tokens.css";
 import "./settings-redesign.css";
 import "./platforms/ipad/ipad.css";
 import "./utility-controls.css";
